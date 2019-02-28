a278f1c2762022b53cc05dd1bf5f9c56e04fe464
Miraç Tok <mirac_tok@hotmail.com>
2019-02-28 11:58:56 +0100

    fon d'ecran dynalique test

---
+++ script.js
@@ -0,0 +1,20 @@
+var bgImageArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg"],
+base = "img/wallpapers/",
+secs = 4;
+bgImageArray.forEach(function(img){
+    new Image().src = base + img; 
+    // caches images, avoiding white flash between background replacements
+});
+
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+			document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			document.documentElement.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+backgroundSequence();
