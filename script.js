c21d2debf8fa5d9446b10aabc2fef8b5ca00b3b5
Miraç Tok <mirac_tok@hotmail.com>
2019-03-19 14:19:59 +0100

    fade complet

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var bgImageArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg","05.jpg","06.jpg","07.jpg","08.jpg","09.jpg"],
 base = "img/wallpapers/",
-secs = 2;
+secs = 6;
 bgImageArray.forEach(function(img){
 	new Image().src = base + img; 
     // caches images, avoiding white flash between background replacements
@@ -12,7 +12,7 @@ function backgroundSequence() {
 	for (i = 0; i < bgImageArray.length; i++) {
 		setTimeout(function(){ 
 			document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
-			document.documentElement.style.backgroundSize ="cover";
+            document.documentElement.style.backgroundSize ="cover";   
 		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
 		}, (secs * 1000) * i)	
 	}
@@ -20,25 +20,3 @@ function backgroundSequence() {
 backgroundSequence();
 
 
-function fadeImage() {
-    $('img').first().fadeOut(2000, function suivante() {
-        $(this).next('img').fadeOut(2000,suivante);
-    });
-  }
-
-
-  function fadeOutEffect() {
-    var fadeTarget = document.getElementById("target");
-    var fadeEffect = setInterval(function () {
-        if (!fadeTarget.style.opacity) {
-            fadeTarget.style.opacity = 1;
-        }
-        if (fadeTarget.style.opacity > 0) {
-            fadeTarget.style.opacity -= 0.1;
-        } else {
-            clearInterval(fadeEffect);
-        }
-    }, 200);
-}
-
-document.getElementById("target").addEventListener('click', fadeOutEffect);
