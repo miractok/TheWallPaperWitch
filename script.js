72cc59623fcb62a741345a89e98ece6dcf3bd580
Elios50 <eliosfilip@yahoo.fr>
2019-03-20 17:51:45 +0100

    Auto stash before merge of "master" and "origin/master"

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var bgImageArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg","05.jpg","06.jpg","07.jpg","08.jpg","09.jpg"],
 base = "img/wallpapers/",
+<<<<<<< Updated upstream
 secs = 6;
+=======
+secs = 2;
+
+>>>>>>> Stashed changes
 bgImageArray.forEach(function(img){
 	new Image().src = base + img; 
     // caches images, avoiding white flash between background replacements
